0196991b4e6f43869cca2bd33a2647a8bf697039
Jason Nye <eynnosaj@hotmail.com>
2017-03-14 13:24:18 -0500

    initial CORS support in proxy

--- authProxy/server/authProxy.js
+++ authProxy/server/authProxy.js
@@ -11,14 +11,27 @@ const proxy = httpProxy.createProxyServer({});
 console.log('Running proxy...');
 
 const server = http.createServer((req, res) => {
+  if (req.method === 'OPTIONS') {
+    return handleOptionsRequest(req, res);
+  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
   const continueOn = checkBlockChain(req);
   if (continueOn) {
     proxy.web(req, res, opts);
   } else {
-    res.statusCode= 403;
+    res.statusCode = 403;
     res.end();
   }
 }).listen(9042);
+
+function handleOptionsRequest(req, res) {
+  res.setHeader('Access-Control-Allow-Methods', 'DELETE, GET, HEAD, OPTIONS, PATCH, POST, PUT');
+  const allowedHeaders = req['access-control-allow-headers'];
+  if (allowedHeaders) {
+    res.setHeader('Access-Control-Allow-Headers', allowedHeaders);
+  }
+  res.setHeader('Access-Control-Max-Age', '86400');
+}
 
 function checkBlockChain(req) {
   const xval = req.headers['x-special-proxy-header'];
