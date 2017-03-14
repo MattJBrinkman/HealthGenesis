3b9d9907dfc0380be1c97ecbdeecd16446cffe46
Chris Hafey <chafey@gmail.com>
2017-03-14 13:37:19 -0400

    fixed test contract creation

---
+++ phrApp/client/ethereumLogin.js
@@ -0,0 +1,21 @@
+import { Connect } from 'uport-connect'
+
+let uport = new Connect('MyDApp')
+
+Template.ethereumLogin.helpers({
+});
+
+Template.ethereumLogin.events({
+  'click #uportLogon'(event, instance) {
+    console.log('click..');
+    uport.requestCredentials().then((credentials) => {
+      console.log(credentials);
+      web3.eth.sign(web3.eth.defaultAccount, "test", (err, res) => {
+        console.log(err, res);
+      });
+    });
+  },
+  'click #web3Logon'(event, instance) {
+    },
+
+});
