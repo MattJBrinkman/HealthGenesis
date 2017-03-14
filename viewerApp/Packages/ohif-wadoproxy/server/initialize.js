c361ee59afaf34c4da7b024e1dc5bc4206a2f9df
JK <jklotzer@nucleushealth.io>
2017-03-14 16:16:08 -0500

    added proxy use for ohif

--- viewerApp/Packages/ohif-wadoproxy/server/initialize.js
+++ viewerApp/Packages/ohif-wadoproxy/server/initialize.js
@@ -2,7 +2,7 @@ import { OHIF } from 'meteor/ohif:core';
 
 Settings = Object.assign({
     uri : OHIF.utils.absoluteUrl("/__wado_proxy"),
-    enabled: false
+    enabled: true
 }, (Meteor.settings && Meteor.settings.proxy) ? Meteor.settings.proxy : {});
 
 http = require("http");
