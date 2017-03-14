1818b4a987d776b170c816d91e839fbda4b68880
Chris Hafey <chafey@gmail.com>
2017-03-14 12:20:43 -0400

    created test smart contract instance

--- shareApp/server/main.js
+++ shareApp/server/main.js
@@ -1,12 +1,11 @@
 import { Meteor } from 'meteor/meteor';
 import resource from '../externals/resource';
 import web3 from '../externals/web3';
+import getOrCreateTestInstanceAddress from './getOrCreateTestInstanceAddress';
 
 Meteor.startup(() => {
   // code to run on server at startup
 
-  const instance = resource.new('localhost:3200/dicomweb/studyUid', web3.eth.accounts[0]);
-
-
-
+  var address = getOrCreateTestInstanceAddress();
+  console.log('using test instance @ ', address);
 });
