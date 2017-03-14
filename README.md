b6e4fbdfd9a062d688c20dd76ceceb400887d50f
Matt Brinkman <mbrinkman@statrad.com>
2017-03-14 10:00:09 -0500

    Initial proxy pass

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ This repository contains the source code for Health Genesis - a project created
 during the ONC Blockchain Hackathon March 14/15 2017.  The goal of the Health
 Genesis project is to show how medical records can be shared using Blockchain
 technology with a demonstration using medical images.
+
+# Run the proxy
+node authProxy.js
+
+Connect to Orthanc via port 9042. when proxy is running.
+ 
---
+++ authProxy/authProxy.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+// Proxy server which checks the block chain permission and then proxies the request
+const opts = { target: 'http://localhost:8042'};
+
+const proxy = httpProxy.createProxyServer({});
+
+const server = http.createServer((req, res) => {
+  const continueOn = checkBlockChain(req);
+  if (continueOn) {
+    proxy.web(req, res, opts);
+  } else {
+    res.statusCode= 403;
+    res.end();
+  }
+}).listen(9042);
+
+function checkBlockChain(req) {
+  const xval = req.headers['x-special-proxy-header'];
+  return (xval && xval === 'true');
+}
