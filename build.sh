2055b933a4f070fd0bb214c3f053fe66299da68b
Matt Brinkman <mbrinkman@statrad.com>
2017-03-14 22:54:40 -0500

    Added viewer to Docker

--- build.sh
+++ build.sh
@@ -7,8 +7,15 @@ function buildApp {
   popd
 }
 
-declare -a apps=("authProxy" "phrApp" "shareApp" "viewerApp")
+declare -a apps=("authProxy" "phrApp" "shareApp")
 for app in "${apps[@]}"
 do
   buildApp $app
 done
+
+pushd viewerApp/OHIFViewer
+echo "Building viewer"
+npm install
+METEOR_PACKAGE_DIRS="../Packages"
+meteor build --directory /tmp/viewerApp/build
+popd 
